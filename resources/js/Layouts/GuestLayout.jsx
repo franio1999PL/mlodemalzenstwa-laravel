f2bc1409e3af2fe2f1521f73c6ba1b1d7f0920ec
Franciszek Sikora <sikorafranek@gmail.com>
2024-05-16 18:19:10 +0200

    fixed links

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,13 +1,12 @@
 import ApplicationLogo from "@/Components/ApplicationLogo";
-import { Link } from "@inertiajs/react";
 
 export default function Guest({ children }) {
     return (
         <div className="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
             <div>
-                <Link href="/">
+                <a href="/">
                     <ApplicationLogo className="w-20 h-20 text-gray-500 fill-current" />
-                </Link>
+                </a>
             </div>
 
             <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
